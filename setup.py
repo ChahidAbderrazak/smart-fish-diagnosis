25b32add978422266b14217175f59298031e2586
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2024-11-11 22:09:24 -0500

    up

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     extras_require={
         "test": ["pytest"],
     },
-    package_dir={"": "src"},
+    package_dir={"": "."},
     packages=find_packages(where="src"),
     classifiers=[
         "Development Status :: 3 - Alpha",
