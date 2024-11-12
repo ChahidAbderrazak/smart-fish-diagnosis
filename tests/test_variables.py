aeb505d915f1efa79a213083d98970439ed3dd5d
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2024-11-11 21:05:54 -0500

    up

--- tests/test_variables.py
+++ tests/test_variables.py
@@ -1,9 +1,8 @@
-import sys
+# import sys
 
 import numpy as np
 
-sys.path.append("src")
-
+# sys.path.append("src")
 from lib.Autils_Object_detection import load_class_dict
 from lib.networks import get_model_instance
 from lib.utils import extract_experiment_parameters
